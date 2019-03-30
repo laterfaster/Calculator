2f47a418625db64cb0d4d29318d6a346b26f14ac
parhelion <34838476+laterfaster@users.noreply.github.com>
2019-03-30 17:13:25 +0800

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,14 +25,13 @@ namespace exp6lib
                             if (!t)
                                 flag = false;
                         }*/
-            if (s[0] == "*" || s[0] == "/" || s[0] == ")")
+            if (s[0] == "*" || s[0] == "/")
                 return false;
             else
                 return true;
         }//ResultOf前运行
         static double Handle(double a, double b, string c)
         {
-            
             if (c == "+")
                 return a + b;
             if (c == "-")
@@ -42,17 +41,12 @@ namespace exp6lib
             if (c == "/")
             {
                 if (a == 0)
-                {
-                    wrong = true;
-                    return 0;
-                }
+                    throw new ArgumentException("除数不能为0");
                 else return b / a;
             }//栈逆序输出，先输出除数a，再输出被除数b
             return 0;
         }
         static int pr(string s) { if (s == "+" || s == "-") return 1; if (s == "*" || s == "/") return 2;  return - 1; }
-        private static bool flag = false;
-        public static bool wrong = false;
         public static double ResultOf(string[] s)
         {
             nums = new Stack<double>(s.Length);
@@ -68,79 +62,28 @@ namespace exp6lib
                         ops.Push(s[index]);
                     else
                     {
-                        while (ops.Count!=0&&pr(ops.Peek()) >= 2)
-                        {
-                            if (nums.Count < 2|| ops.Count == 0)
-                            {
-                                wrong = true;
-                                return 0;
-                            }
+                        while(pr(ops.Peek()) >= 2 )
                             nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
-                        }
                         ops.Push(s[index]);
                     }
                 if (s[index] == "+" || s[index] == "-")
                 {
-                    if (s[index] == "-" && (index==0 || s[index - 1] == "("))
-                    {
-                        double n;
-                        if (double.TryParse(s[index + 1], out n))
-                        { nums.Push(-n); index++; }
-                        else if (s[index + 1] == "(")
-                            flag = true;
-                    }
-
-                    else
-                    {
-                        while (ops.Count != 0 && ops.Peek() != "(")
-                        {
-                            if (nums.Count < 2||ops.Count == 0)
-                            {
-                                wrong = true;
-                                return 0;
-                            }
-                            nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
-                        }
-                        ops.Push(s[index]);
-                    }
+                    while (ops.Count != 0&&ops.Peek()!="(")
+                        nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
+                    ops.Push(s[index]);
                 }
                 if (s[index] =="(")
                     ops.Push(s[index]);
                 if (s[index] == ")")
                 {
                     while (ops.Peek() != "(")
-                    {
-                        if (nums.Count < 2||ops.Count == 0)
-                        {
-                            wrong = true;
-                            return 0;
-                        }
                         nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
-                    }
                     ops.Pop();
-                    if (flag)
-                    {
-                        double t = nums.Pop();
-                        nums.Push(-t);
-                        flag = false;
-                    }
                 }
                 index++;
             }
-            while (ops.Count != 0)
-            {
-                if (nums.Count < 2)
-                {
-                    wrong = true;
-                    return 0;
-                }
+            while(ops.Count != 0)
                 nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
-            }
-            if(nums.Count==0)
-            {
-                wrong = true;
-                return 0;
-            }
             return nums.Peek();
         }
 
