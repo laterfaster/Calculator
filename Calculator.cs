6ba49fdb3164f2dda99980de018334d14d573c24
parhelion <34838476+laterfaster@users.noreply.github.com>
2019-03-30 16:38:02 +0800

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exp6lib
+{
+    public class Calculator
+    {
+        static Stack<string> ops;
+        static Stack<double> nums;
+        static string[] oprators = { "+", "-", "*", "/" ,"(",")"};
+        public static bool Check(string[] s)
+        {
+            /*            bool flag = true;
+                        char[] legal = new char[]{ '+', '-', '*', '/', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                        foreach (char c in s)//只要有一个不在集合中，flag为false
+                        {
+                            bool t = false;
+                            for (int i = 0; i < legal.Length; i++)//判断字符c是否在集合中
+                                if (c == legal[i])
+                                    t = true;
+                            //t为“c在集合中？”
+                            if (!t)
+                                flag = false;
+                        }*/
+            if (s[0] == "*" || s[0] == "/" || s[0] == ")")
+                return false;
+            else
+                return true;
+        }//ResultOf前运行
+        static double Handle(double a, double b, string c)
+        {
+            
+            if (c == "+")
+                return a + b;
+            if (c == "-")
+                return b - a;
+            if (c == "*")
+                return a * b;
+            if (c == "/")
+            {
+                if (a == 0)
+                {
+                    wrong = true;
+                    return 0;
+                }
+                else return b / a;
+            }//栈逆序输出，先输出除数a，再输出被除数b
+            return 0;
+        }
+        static int pr(string s) { if (s == "+" || s == "-") return 1; if (s == "*" || s == "/") return 2;  return - 1; }
+        private static bool flag = false;
+        public static bool wrong = false;
+        public static double ResultOf(string[] s)
+        {
+            nums = new Stack<double>(s.Length);
+            ops = new Stack<string>(s.Length);
+            int index = 0;
+            while (index < s.Length)
+            {
+                double d;
+                if (double.TryParse(s[index], out d))
+                    nums.Push(d);
+                if (s[index] == "*" || s[index] == "/")
+                    if (ops.Count == 0)
+                        ops.Push(s[index]);
+                    else
+                    {
+                        while (ops.Count!=0&&pr(ops.Peek()) >= 2)
+                        {
+                            if (nums.Count < 2|| ops.Count == 0)
+                            {
+                                wrong = true;
+                                return 0;
+                            }
+                            nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
+                        }
+                        ops.Push(s[index]);
+                    }
+                if (s[index] == "+" || s[index] == "-")
+                {
+                    if (s[index] == "-" && (index==0 || s[index - 1] == "("))
+                    {
+                        double n;
+                        if (double.TryParse(s[index + 1], out n))
+                        { nums.Push(-n); index++; }
+                        else if (s[index + 1] == "(")
+                            flag = true;
+                    }
+
+                    else
+                    {
+                        while (ops.Count != 0 && ops.Peek() != "(")
+                        {
+                            if (nums.Count < 2||ops.Count == 0)
+                            {
+                                wrong = true;
+                                return 0;
+                            }
+                            nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
+                        }
+                        ops.Push(s[index]);
+                    }
+                }
+                if (s[index] =="(")
+                    ops.Push(s[index]);
+                if (s[index] == ")")
+                {
+                    while (ops.Peek() != "(")
+                    {
+                        if (nums.Count < 2||ops.Count == 0)
+                        {
+                            wrong = true;
+                            return 0;
+                        }
+                        nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
+                    }
+                    ops.Pop();
+                    if (flag)
+                    {
+                        double t = nums.Pop();
+                        nums.Push(-t);
+                        flag = false;
+                    }
+                }
+                index++;
+            }
+            while (ops.Count != 0)
+            {
+                if (nums.Count < 2)
+                {
+                    wrong = true;
+                    return 0;
+                }
+                nums.Push(Handle(nums.Pop(), nums.Pop(), ops.Pop()));
+            }
+            if(nums.Count==0)
+            {
+                wrong = true;
+                return 0;
+            }
+            return nums.Peek();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WindowsCalculator.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using exp6lib;
+
+namespace exp6app
+{
+    public partial class WindowsCalculator : Form
+    {
+        public WindowsCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label1.Text += "8";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label1.Text += "9";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            label1.Text += "00";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text += "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            label1.Text += ".";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text.Substring(0, label1.Text.Length - 1);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            label1.Text += " + ";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            label1.Text += " - ";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            label1.Text += " * ";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            label1.Text += " / ";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            label1.Text += " ( ";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            label1.Text += " ) ";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            char[] charSeparators = new char[] { ' ' };
+            string[] target = label1.Text.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries);
+            if (Calculator.Check(target))
+                label1.Text = Calculator.ResultOf(target).ToString();
+            else label1.Text = "非法输入";
+            if(Calculator.wrong)
+                label1.Text = "非法输入";
+
+        }
+    }
+}
